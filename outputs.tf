dff1e17f5fe6a33503f31795ffcfbd071bb38afb
Afraz Ahmadzadeh <afrazo@proton.me>
2024-09-04 13:16:21 +0200

    WIP: Changed keys for dyanmodb_table_stream_arns to only include original name

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "dyanmodb_table_stream_arns" {
+  value = { for this_name, these_values in module.dynamodb_table : this_name => these_values.dynamodb_table_stream_arn }
+}
+
+output "dyanmodb_table_stream_arns_no_app_name_prefix" {
   value = { for this_name, these_values in module.dynamodb_table : trim(this_name, var.application_name) => these_values.dynamodb_table_stream_arn }
 }
