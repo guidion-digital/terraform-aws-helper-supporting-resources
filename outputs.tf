c3d9e6987b6fb6e0fece530d7f8f6ceb73cda4cd
Afraz Ahmadzadeh <afrazo@proton.me>
2024-09-04 15:50:32 +0200

    WIP: Need to take namespace back into account

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "dyanmodb_table_stream_arns" {
-  value = { for this_name, these_values in module.dynamodb_table : this_name => these_values.dynamodb_table_stream_arn }
+  value = { for this_name, these_values in module.dynamodb_table : (var.namespacing_enabled ? "${var.application_name}-${this_name}" : this_name) => these_values.dynamodb_table_stream_arn }
 }
 
 output "dyanmodb_table_stream_arns_no_app_name_prefix" {
