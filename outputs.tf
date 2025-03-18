f63634f38fb5d78af720697fdffbe85811da8f4b
Afraz Ahmadzadeh <afrazo@proton.me>
2025-03-18 11:39:13 +0100

    feat: Adding whole outputs of both resources

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "dyanmodb_table_stream_arns" {
 output "dyanmodb_table_stream_arns_no_app_name_prefix" {
   value = { for this_name, these_values in module.dynamodb_table : trim(this_name, var.application_name) => these_values.dynamodb_table_stream_arn }
 }
+
+output "dynamodb" {
+  value = module.dynamodb_table
+}
+
+output "sqs" {
+  value = var.sqs_queues
+}
